cbc5cfe0510346017162113fde486bc80bb04020
ghulam1052 <ghulam1052@gmail.com>
2026-03-27 21:08:49 +0400

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+// v2
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/validation-key.txt",
+        destination: "/api/validation-key",
+      },
+    ]
+  },
+}
+
+export default nextConfig
